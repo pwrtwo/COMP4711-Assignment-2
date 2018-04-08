536933c2538f545154864b9252d742dfe2c93fd6
ianianda <ywang402@my.bcit.ca>
2018-04-08 12:55:24 -0700

    added more tests

--- tests/AccessoryTest.php
+++ tests/AccessoryTest.php
@@ -4,15 +4,41 @@ class AccessoryTest extends TestCase
 {
     private $CI;
     private $accessory;
+    
     public function setUp()
     {
         // Load CI instance normally
         $this->CI = &get_instance();
-        $this->CI->load->model('accessory');
+        $this->CI->load->model('Accessory');
         $this->accessory = new Accessory;
     }
+    
     public function testAccessoryName()
     {
-        $this->accessory->setName("abc");
+        $expected = "Justin Bieber";
+        $this->accessory->setName($expected);
+        $this->assertEquals($expected, $this->accessory->getName());
     }
+    
+    public function testInvalidProfessionalityAbsent()
+    {
+        $expected = "";
+        $this->expectException(Exception::class);
+        $this->accessory->setProfessionality($expected);
+    }
+    
+    public function testInvalidWeightNegative()
+    {
+        $expected = -5;
+        $this->expectException(Exception::class);
+        $this->accessory->setWeight($expected);
+    }
+    
+      public function testValidWeight()
+    {
+        $expected = 5;
+        $this->accessory->setWeight($expected);
+        $this->assertEquals($expected, $this->accessory->getWeight());
+    }
+    
 }
